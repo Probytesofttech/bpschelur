d2b8d366f2a6d8fc833a41b9f44c0ac587f00717
suguna.mallikarjun <suguna.mallikarjun@gmail.com>
2018-10-14 14:46:22 +0530

    Initial create

---
+++ WebApplication8/Controllers/SchoolController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Curriculum()
+        {
+            return View();
+        }
+
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult Facilities()
+        {
+            return View();
+        }
+
+    }
+}
